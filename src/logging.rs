3db584ff54a696635ade893a3d800577fe9c46ee
Tjardo <info@tjardo.com>
2023-12-08 14:28:13 +0100

    Formatting

--- src/logging.rs
+++ src/logging.rs
@@ -4,10 +4,12 @@ pub fn setup_logging() -> Result<(), Box<dyn Error>> {
     fern::Dispatch::new()
         .format(|out, message, record| {
             out.finish(format_args!(
-                "[{} {} {}] {}",
+                "{} {} [{}] [{}:{}] {}",
                 chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
                 record.level(),
                 record.target(),
+                record.file().unwrap_or("unknown"),
+                record.line().unwrap_or(0),
                 message
             ))
         })
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,13 +2,13 @@ const { fontFamily } = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./templates/*.html"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Noto Sans", ...fontFamily.sans],
-      },
-    },
-  },
-  plugins: [],
+	content: ["./templates/*.html"],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["Noto Sans", ...fontFamily.sans],
+			},
+		},
+	},
+	plugins: [],
 };
