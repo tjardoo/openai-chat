cfac7e8707b13f65bdf7bd485e32c9fc42094e12
Tjardo <info@tjardo.com>
2023-12-10 17:22:33 +0100

    Add custom struct/field validation error

--- src/http/validation.rs
+++ src/http/validation.rs
@@ -11,19 +11,36 @@ use validator::Validate;
 pub struct ValidatedJson<T>(pub T);
 
 #[derive(Debug)]
-pub struct ValidationError;
+pub struct ValidationErrors {
+    errors: ValidationError,
+}
 
-impl std::fmt::Display for ValidationError {
+#[derive(Debug)]
+pub enum ValidationError {
+    Struct(String),
+    Field(String),
+}
+
+impl std::fmt::Display for ValidationErrors {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         "Server error".fmt(f)
     }
 }
 
-impl Error for ValidationError {}
+impl std::fmt::Display for ValidationError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ValidationError::Struct(message) => write!(f, "Struct: {}", message),
+            ValidationError::Field(message) => write!(f, "Field: {}", message),
+        }
+    }
+}
 
-impl IntoResponse for ValidationError {
+impl Error for ValidationErrors {}
+
+impl IntoResponse for ValidationErrors {
     fn into_response(self) -> axum::response::Response {
-        (StatusCode::BAD_REQUEST, "Incorrect JSON").into_response()
+        (StatusCode::BAD_REQUEST, self.errors.to_string()).into_response()
     }
 }
 
@@ -34,14 +51,26 @@ where
     S: Send + Sync,
     T: Validate,
 {
-    type Rejection = ValidationError;
+    type Rejection = ValidationErrors;
 
     async fn from_request(req: Request, state: &S) -> Result<Self, Self::Rejection> {
-        let Json(json) = Json::<T>::from_request(req, state)
-            .await
-            .map_err(|_| ValidationError)?;
+        let Json(json) =
+            Json::<T>::from_request(req, state)
+                .await
+                .map_err(|error| ValidationErrors {
+                    errors: ValidationError::Struct(error.body_text().to_string()),
+                })?;
 
-        json.validate().map_err(|_| ValidationError)?;
+        json.validate().map_err(|errors| ValidationErrors {
+            errors: ValidationError::Field(
+                errors
+                    .errors()
+                    .into_iter()
+                    .map(|(key, _value)| key.to_string())
+                    .collect::<Vec<_>>()
+                    .join(", "),
+            ),
+        })?;
 
         Ok(ValidatedJson(json))
     }
