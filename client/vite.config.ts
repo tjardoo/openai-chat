67d8b4a6dcfe24b0b7007a7ece1311b328828998
Tjardo <info@tjardo.com>
2023-12-27 20:28:35 +0100

    Set Vite strict port

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -9,5 +9,9 @@ export default defineConfig({
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url))
 		}
-	}
+	},
+    server: {
+        strictPort: true,
+        port: 5173
+    }
 })
